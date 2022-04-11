933c1a6dbf2b5c939f0f9e0670177b4557ff2b14
1318PK <ainur.hak2004@gmail.com>
2022-04-11 15:51:05 +0300

    repair app.xaml

--- ShopShakirov/App.xaml.cs
+++ ShopShakirov/App.xaml.cs
@@ -15,37 +15,6 @@ namespace ShopShakirov
     /// </summary>
     public partial class App : Application
     {
-        private void TextSizeChanger(object sender, SizeChangedEventArgs e)
-        {
-            //Size n = e.NewSize;
-            //Size p = e.PreviousSize;
-            //double l = n.Width / p.Width;
-            //if (l != double.PositiveInfinity)
-            //{
-            //    if (sender is TextBox)
-            //        (sender as TextBox).FontSize *= l;
-            //    if (sender is PasswordBox)
-            //        (sender as PasswordBox).FontSize *= l;
-            //    else if (sender is TextBlock)
-            //        (sender as TextBlock).FontSize *= l;
-            //    else if (sender is Button)
-            //        (sender as Button).FontSize *= l;
-            //    else if (sender is CheckBox)
-            //    {
-            //        ScaleTransform scale = new ScaleTransform(2, 2);
-            //        if (l < 1)
-            //        {
-            //            scale.ScaleX *= l / 4;
-            //            scale.ScaleY *= l / 4;
-            //        }
-            //        else
-            //        {
-            //            scale.ScaleX *= l;
-            //            scale.ScaleY *= l;
-            //        }
-            //        (sender as CheckBox).RenderTransform = scale;
-            //    }
-            //}
-        }
+        
     }
 }
