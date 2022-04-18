f960ee5137749c17a9a6bbbd417be1edcd29a36b
Ainur <ainur.hak2004@gmail.com>
2022-04-19 02:10:28 +0300

    registration correct work

--- ShopShakirov/Pages/RegisterPage.xaml.cs
+++ ShopShakirov/Pages/RegisterPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,6 +21,7 @@ namespace ShopShakirov.Pages
     /// </summary>
     public partial class RegisterPage : Page
     {
+        int clientRoleId = 3;
         public RegisterPage()
         {
             InitializeComponent();
@@ -27,7 +29,30 @@ namespace ShopShakirov.Pages
 
         private void BtnRegisterClick(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new LoginPage());
+            var login = tbxLogin.Text;
+            var password = pbxPassword.Password;
+            var users = MainWindow.dbConnection.User;
+
+            if (users.Where(a => a.Login == login).Count() != 0)
+            {
+                MessageBox.Show("Данный логин уже занят", "Ошибка");
+            }
+            else if (!new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^])(?=.*[^a-zA-Z0-9])\S{6,16}$").IsMatch(password))
+            {
+                MessageBox.Show("Неверный формат пароля", "Ошибка");
+            }
+            else
+            {
+                User newUser = new User();
+                newUser.Login = login;
+                newUser.Password = password;
+                newUser.RoleId = clientRoleId;
+                users.Add(newUser);
+                MainWindow.dbConnection.SaveChanges();
+                MessageBox.Show("Вы успешно зарегестрировались!");
+                NavigationService.Navigate(new LoginPage());
+            }
+            
         }
     }
 }
